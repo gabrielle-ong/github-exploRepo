b6d8458458a9b40943b78e1e4cb8919a0391dd11
Gabrielle Ong <gabrielleong97@gmail.com>
2016-09-02 16:25:20 +0800

    got ajax search working

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-angular.module('repoApp', ['ngMaterial']);
+angular.module('repoApp', [])
+  .controller("Ctrl", function ($scope,$http){
+    $scope.search = function(){
+      console.log($scope.searchString)
+      $http.get('https://api.github.com/search/repositories?q='+$scope.searchString)
+        .success(function(data) {
+          console.log(data.total_count)
+          $scope.results = data.items;
+        })
+        .error(function(data) {
+          console.log("No data found..");
+        })
+    }
+  })
