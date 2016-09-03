f30c78db1306648795cb8797a1f77e48bdd62a8b
Gabrielle Ong <gabrielleong97@gmail.com>
2016-09-03 12:59:06 +0800

    optimised ajax calls, no need for 3

--- app.js
+++ app.js
@@ -2,32 +2,24 @@ angular.module('repoApp', [])
   .controller('Ctrl', function ($scope, $http) {
     $scope.search = function () {
       $http.get('https://api.github.com/search/repositories?q=' + $scope.searchString)
+        // Going to assume its watchers instead of followers because owners have followers while repos have watchers
         .success(function (data) {
           $scope.results = data.items
         })
         .error(function (data) {
-          $scope.searchErrorMessage='No repos found...'
+          $scope.searchErrorMessage = 'No repos found...'
         })
     }
 
     $scope.expand = function (repoFullName) {
       console.log(repoFullName + 'was clicked!')
-      $http.get('https://api.github.com/repos/' + repoFullName)
+      $http.get('https://api.github.com/repos/' + repoFullName + '/languages')
         .success(function (data) {
-          console.log(data.description)
-          //Going to assume its watchers instead of followers because owners have followers while repos have watchers
-          console.log(data.watchers)
-          console.log(data.html_url)
+          console.log(data)
+          $scope.language = data
         })
-        .error(function (data) {watchers
-          console.log('Could not get repo details')
+        .error(function (data) {
+          console.log('Could not get repo languages')
         })
-        $http.get('https://api.github.com/repos/' + repoFullName + '/languages')
-          .success(function (data) {
-            console.log(data)
-          })
-          .error(function (data) {
-            console.log('Could not get repo languages')
-          })
     }
   })
